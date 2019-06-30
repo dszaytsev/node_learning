e131a139fb53974888d997f25a521eb68d8cf5d8
Dmitry <zadima@gmail.com>
2019-06-30 21:52:29 +0300

    login form

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,20 @@
+const db = require('../services/db')
+
 exports.get = (ctx, next) => {
-  ctx.render('login')
+  ctx.render('login', { error: ctx.flash.get() })
 }
 
 exports.post = (ctx, next) => {
-  debugger
+  const { email, password } = ctx.request.body
+
+  const userExists = db.user().isEqual({ email, password }).value()
+
+  if (!email || !password) ctx.flash.set('Все поля обязательны для заполнения')
+  else if (!userExists) ctx.flash.set('Введен неверный логин или пароль')
+  else {
+    ctx.session.isAdmin = true
+    return ctx.redirect('/admin')
+  }
+
+  ctx.redirect('/login')
 }
--- routes/login.js
+++ routes/login.js
@@ -2,5 +2,6 @@ const login = new require('koa-router')()
 const loginController = require('../controllers/login')
 
 login.get('/', loginController.get)
+login.post('/', loginController.post)
 
 module.exports = login
