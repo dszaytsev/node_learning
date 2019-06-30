3ec2a350359c78531def2a369b806925c397e5b1
Dmitry <zadima@gmail.com>
2019-06-30 13:51:37 +0300

    send mail from index page

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,5 +2,6 @@ const homeController = require('../controllers/home')
 const home = require('express').Router()
 
 home.get('/', homeController.get)
+home.post('/', homeController.post)
 
 module.exports = home
